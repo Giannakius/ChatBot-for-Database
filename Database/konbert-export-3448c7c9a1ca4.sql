adf094eaea9145d57816f4bb452fbcddfedc4d90
John Tournis <giannakius10@gmail.com>
2024-04-21 23:03:09 +0300

    Final Comit

---
+++ Database/konbert-export-3448c7c9a1ca4.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `mytable` (
+  `Debit;Credit;Balance;Inflows Forecast;Outflows Forecast;Projected Ballance` VARCHAR(1024)
+);
+
+INSERT INTO `mytable` (`Debit;Credit;Balance;Inflows Forecast;Outflows Forecast;Projected Ballance`)
+VALUES
+('300000;450000;150000;15000000;9000000;23850000');
---
+++ Database/konbert-export-82d6ffc04a8b4.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `mytable` (
+  `Channel;Orders Count;Order Lines;Quantity in pieces;Quantity in boxes;Amount;% of fulfillment` VARCHAR(1024)
+);
+
+INSERT INTO `mytable` (`Channel;Orders Count;Order Lines;Quantity in pieces;Quantity in boxes;Amount;% of fulfillment`)
+VALUES
+('Online Retailers;520;3683;5160;746;3642673;98'),
+('Retail Stores;1170;4150;4632;667;3513708;97'),
+('Web Site;270;350;396;56;284790;99'),
+('Wholesalers;355;5100;6783;956;5328397;96');
